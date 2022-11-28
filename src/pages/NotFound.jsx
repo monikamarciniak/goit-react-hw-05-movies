956579d7274062163b1248c87c79388ddda92fcb
Monika Marciniak <monixxa90@gmail.com>
2022-11-28 13:56:40 +0100

    changes

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <main style={{ textAlign: 'center' }}>
+      <b style={{ fontSize: 64 }}>404</b>
+      <p>Ups... something get wrong, this page doesn't exist :(</p>
+    </main>
+  );
+};
+
+export default NotFound;
